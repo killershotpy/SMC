f43a924ce6dba59440aab8d96c3b8e2e04eaba22
python_3 <killershotpy>
2024-04-10 23:18:32 +0300

    v 1.1.3 update's: - edit port loads

--- server_options.py
+++ server_options.py
@@ -3,9 +3,17 @@ import os
 from json import loads as ld
 
 
+name_file_config = 'options_server'
 default_host = 'host = \"127.0.0.1\"'
 default_port = 'port = 20001'
 default_host_port = [default_host, default_port]
+
+
+try:
+    loaded_config = {s.split()[0]: ld(s.split()[2]) if len(s.split()) == 3 else {} for s in open(name_file_config, 'r', encoding='utf-8').readlines()}
+except FileNotFoundError:
+    open(name_file_config, 'w', encoding='utf-8').write('\n'.join(default_host_port))
+    loaded_config = {s.split()[0]: ld(s.split()[2]) if len(s.split()) == 3 else {} for s in open(name_file_config, 'r', encoding='utf-8').readlines()}
 
 
 class ParametersNames:
@@ -16,15 +24,9 @@ class ParametersNames:
     call_func = 'call_func'
     data = 'data'
     name_thread_cleaner = 'cleaner'
-    name_file_config = 'options_server'
     timeout = 'timeout'
-    multiproc_ports_list = [int(default_host_port[1].split()[2]) + x for x in range(os.cpu_count() * 2)]
-    loaded_config = None
-    try:
-        loaded_config = {s.split()[0]: ld(s.split()[2]) if len(s.split()) == 3 else {} for s in open(name_file_config, 'r', encoding='utf-8').readlines()}
-    except FileNotFoundError:
-        open(name_file_config, 'w', encoding='utf-8').write('\n'.join(default_host_port))
-        loaded_config = {s.split()[0]: ld(s.split()[2]) if len(s.split()) == 3 else {} for s in open(name_file_config, 'r', encoding='utf-8').readlines()}
+    loaded_config = loaded_config
+    multiproc_ports_list = [loaded_config['port'] + x for x in range(os.cpu_count() * 2)]
 
     def __setattr__(self, *args, **kwargs): raise NotImplementedError('can\'t rewrite attributes')
     def __delete__(self, instance): raise NotImplementedError('it is not possible to delete this configuration class of an application')
